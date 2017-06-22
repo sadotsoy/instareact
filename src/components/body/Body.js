d3fc4d36d72a5cd03c55e5df0d64483a4606bdc9
Sadot Cortes <sadotcortes@scoorm.me>
2017-06-22 11:34:36 -0500

    Navbar, body component added

---
+++ src/components/body/Body.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './body.css';
+
+class Body extends Component {
+	render() {
+	return(
+				<section>
+					<div className ="bdy"></div>
+				</section>
+			);
+	}
+}
+
+export default Body;
